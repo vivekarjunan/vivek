cedd3a8980825b478f423677bbf1de58937de97a
vivekarjunan <vivekarjunan222@gmail.comgit config --global user.email vivekarjunan222@gmail.com>
2023-02-13 17:50:34 +0530

    first program

---
+++ Book.java
@@ -0,0 +1,19 @@
+public class Book{
+
+ static String name;
+ static int fees;
+
+ String readerName;
+
+public static void main(String[]args){
+
+ Book bb = new Book();
+ 
+  bb.readerName = "vivek";
+   
+  System.out.println(bb.readerName);
+
+
+}
+
+}
